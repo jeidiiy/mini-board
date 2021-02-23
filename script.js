79648ec7cd8c4d465aa8a46b4f28bdb4aa6b3e83
wscrg <wldyd2356@gmail.com>
2021-02-23 17:48:42 +0900

    feat: webpack 환경 설정

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 import './style.scss';
+// import 'regenerator-runtime/runtime';
+
+async function fetchData(url) {
+  const response = await fetch(url);
+  const posts = await response.json();
+  console.log(posts);
+}
+
+fetchData('http://localhost:3000/posts');
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './script.js',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+};
