963352e3b15b7f53a59219a1e7bb0aa8aea5d72c
wscrg <wldyd2356@gmail.com>
2021-02-23 18:50:11 +0900

    feat: API 연동

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 import './style.scss';
-// import 'regenerator-runtime/runtime';
+
+const table = document.querySelector('.table');
 
 async function fetchData(url) {
   const response = await fetch(url);
   const posts = await response.json();
-  console.log(posts);
+
+  createList(posts);
 }
 
-fetchData('http://localhost:3000/posts');
+function createList(data) {
+  const dataName = ['id', 'title', 'name', 'date', ' views'];
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < data.length; i++) {
+    const info = data[i];
+    const { id, title, name, date, views } = info;
+    const dataArr = [id, title, name, date, views];
+
+    const tr = document.createElement('tr');
+
+    for (let i = 0; i < dataName.length; i++) {
+      const td = document.createElement('td');
+      td.className = dataName[i];
+      td.textContent = dataArr[i];
+      tr.appendChild(td);
+    }
+
+    fragment.appendChild(tr);
+  }
+
+  table.appendChild(fragment);
+}
+
+const post = fetchData('http://localhost:3000/posts?_page=1');
